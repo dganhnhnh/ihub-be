e1e33131f598d68c33fd75af65124414d51e2c52
duonganh <duonganh2973@gmail.com>
2023-12-31 22:42:42 +0700

    data, schema

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ app = FastAPI()
 
 
 class InputSchema(BaseModel):
-    tax_history: float
-    electricity_consumption: float
-    home_ownership: bool
-
+    no_family_credit_cards: int
+    total_electricity_bills_paid_on_time_last_2_years: int
+    total_electricity_bills_not_paid_on_time_last_2_years: int
+    have_been_laid_off: bool
+    have_been_bankrupted: bool
+    have_violated_laws: bool
+    have_foreclosure: bool
+    loan_amounts_overdue_for_30_59_days_last_2_years: int
+    loan_amounts_overdue_for_60_89_days_last_2_years: int
+    loan_amounts_overdue_for_90_days_above_last_2_years: int
+    loan_amounts_paid_on_time_last_2_years: int
+    average_family_member_credit_accounts_age: int
+    no_new_family_member_credit_accounts_last_2_years: int
+    no_family_houses: int
+    no_family_members: int
+    male: bool
+    education_status: str
+    marriage_status: str
+    tax_in_last_year: int
 
 @app.get("/predict-credit-score")
 def predict_credit_score(
