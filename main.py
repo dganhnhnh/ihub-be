aecd3ce2224ae842281a52f1daac995e7d991ce0
duonganh <duonganh2973@gmail.com>
2024-01-01 16:19:25 +0700

    get info api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import csv
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,25 +7,43 @@ app = FastAPI()
 
 
 class InputSchema(BaseModel):
-    no_family_credit_cards: int
-    total_electricity_bills_paid_on_time_last_2_years: int
-    total_electricity_bills_not_paid_on_time_last_2_years: int
-    have_been_laid_off: bool
-    have_been_bankrupted: bool
-    have_violated_laws: bool
-    have_foreclosure: bool
-    loan_amounts_overdue_for_30_59_days_last_2_years: int
-    loan_amounts_overdue_for_60_89_days_last_2_years: int
-    loan_amounts_overdue_for_90_days_above_last_2_years: int
-    loan_amounts_paid_on_time_last_2_years: int
-    average_family_member_credit_accounts_age: int
-    no_new_family_member_credit_accounts_last_2_years: int
-    no_family_houses: int
-    no_family_members: int
-    male: bool
-    education_status: str
-    marriage_status: str
-    tax_in_last_year: int
+    numberOfFamilyCreditCards: int
+    totalElectricityBillsPaidOnTimeLast2Years: int
+    totalElectricityBillsNotPaidOnTimeLast2Years: int
+    haveBeenLaidOff: bool
+    haveBeenBankrupted: bool
+    haveViolatedLaws: bool
+    haveForeclosure: bool
+    loanAmountsOverdueFor3059DaysInTheLast2Years: int
+    loanAmountsOverdueFor6089DaysInTheLast2Years: int
+    loanAmountsOverdueFor90DaysInTheLast2Years: int
+    numberOfLoanPaidOnTimeInTheLast2Years: int
+    averageFamilyMemberCreditAccountsAge: int
+    numberOfNewFamilyMemberCreditAccountsInTheLast2Years: int
+    numberOfFamilyHouses: int
+    numberOfFamilyMembers: int
+    gender: bool
+    educationStatus: str
+    marriageStatus: str
+    taxInTheLastlYear: int
+
+    
+@app.get("/get-info")
+def get_info() -> dict:
+    csv_file_path = './data.csv'
+
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    header = rows[0]
+    data = rows[1:]
+    rr = random.choice(data)
+    rd = dict(zip(header, rr))
+    # rd without creditStatus attribute
+    del rd['creditStatus']
+    return rd
+
 
 @app.get("/predict-credit-score")
 def predict_credit_score(
